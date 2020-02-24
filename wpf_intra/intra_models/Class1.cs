fae8c69802321a56e82c8643bad21e8fb3b7ab2d
Raphaël Thériault <raphael_theriault@outlook.com>
2020-02-24 13:08:56 -0500

    4

--- wpf_intra/intra_models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace intra_models
-{
-    public class Class1
-    {
-    }
-}
---
+++ wpf_intra/intra_models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace intra_models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name { get => _name; set { _name = value; OnPropertyChanged(); } }
+        private string _lastName;
+        public string LastName { get => _lastName; set { _lastName = value; OnPropertyChanged(); } }
+        private string _address;
+        public string Address { get => _address; set { _address = value; OnPropertyChanged(); } }
+        private string _city;
+        public string City { get => _city; set { _city = value; OnPropertyChanged(); } }
+        private string _province;
+        public string Province { get => _province; set { _province = value; OnPropertyChanged(); } }
+        private string _postalCode;
+        public string PostalCode { get => _postalCode; set { _postalCode = value; OnPropertyChanged(); } }
+        private string _contactInfo;
+        public string ContactInfo { get => _contactInfo; set { _contactInfo = value; OnPropertyChanged(); } }
+        private string _picturePath;
+        public string PicturePath { get => _picturePath; set { _picturePath = value; OnPropertyChanged(); } }
+        private string _info;
+        public string Info { get => _info; set { _info = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
